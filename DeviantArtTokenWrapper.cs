e63425c78807a5e142b4fba8a03081bfd462583a
Isaac Schemm <isaacschemm@gmail.com>
2019-12-22 15:51:57 -0600

    Add wrapper around AuthenticationProperties for DeviantArtFs

---
+++ DeviantArtTokenWrapper.cs
@@ -0,0 +1,29 @@
+﻿using DeviantArtFs;
+using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DANotify {
+    public class DeviantArtTokenWrapper : IDeviantArtAutomaticRefreshToken {
+        private readonly AuthenticationProperties _authenticationProperties;
+
+        public IDeviantArtAuth DeviantArtAuth { get; }
+
+        public DeviantArtTokenWrapper(IDeviantArtAuth auth, AuthenticationProperties authenticationProperties) {
+            DeviantArtAuth = auth ?? throw new ArgumentNullException(nameof(auth));
+            _authenticationProperties = authenticationProperties ?? throw new ArgumentNullException(nameof(authenticationProperties));
+        }
+
+        public string RefreshToken => _authenticationProperties.GetTokenValue("refresh_token");
+
+        public string AccessToken => _authenticationProperties.GetTokenValue("access_token");
+
+        public Task UpdateTokenAsync(IDeviantArtRefreshToken value) {
+            _authenticationProperties.UpdateTokenValue("refresh_token", value.RefreshToken);
+            _authenticationProperties.UpdateTokenValue("access_token", value.AccessToken);
+            return Task.CompletedTask;
+        }
+    }
+}
