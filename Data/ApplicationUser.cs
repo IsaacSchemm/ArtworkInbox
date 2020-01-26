333b96015193569768921596b64831d146b477b2
Isaac Schemm <isaacschemm@gmail.com>
2020-01-25 18:39:28 -0600

    Add WeasylFs

--- Data/ApplicationUser.cs
+++ Data/ApplicationUser.cs
@@ -5,14 +5,17 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using WeasylFs;
 
 namespace ArtworkInbox.Data {
-    public class ApplicationUser : IdentityUser {
+    public class ApplicationUser : IdentityUser, IWeasylCredentials {
         public bool HideReposts { get; set; }
         public bool HideMature { get; set; }
         public bool HideMatureThumbnails { get; set; }
 
         [Column(TypeName = "varchar(max)")]
         public string WeasylApiKey { get; set; }
+
+        string IWeasylCredentials.ApiKey => WeasylApiKey;
     }
 }
