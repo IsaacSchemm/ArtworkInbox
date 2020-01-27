c99ab35088e9cfcecea0c023e31cedcc831d4ab7
Isaac Schemm <isaacschemm@gmail.com>
2020-01-27 14:30:02 -0600

    Hook up OAuth to a Mastodon server

--- Startup.cs
+++ Startup.cs
@@ -14,6 +14,13 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using DeviantArtFs;
 using Tweetinvi.Models;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using System.Text.Json;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
 
 namespace ArtworkInbox {
     public class Startup {
@@ -45,6 +52,51 @@ namespace ArtworkInbox {
                     t.ConsumerKey = Configuration["Authentication:Tumblr:ConsumerKey"];
                     t.ConsumerSecret = Configuration["Authentication:Tumblr:ConsumerSecret"];
                     t.SaveTokens = true;
+                })
+                .AddOAuth("botsin.space", o => {
+                    // https://medium.com/@mauridb/using-oauth2-middleware-with-asp-net-core-2-0-b31ffef58cd0
+
+                    o.ClientId = Configuration["Authentication:botsin.space:ClientId"];
+                    o.ClientSecret = Configuration["Authentication:botsin.space:ClientSecret"];
+
+                    o.AuthorizationEndpoint = "https://botsin.space/oauth/authorize";
+                    o.TokenEndpoint = "https://botsin.space/oauth/token";
+                    o.UserInformationEndpoint = "https://botsin.space/api/v1/accounts/verify_credentials";
+                    o.CallbackPath = new Microsoft.AspNetCore.Http.PathString("/signin-botsin-space");
+
+                    o.Scope.Add("read");
+
+                    o.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+                    o.ClaimActions.MapJsonKey(ClaimTypes.Name, "username");
+                    o.ClaimActions.MapJsonKey("urn:botsin.space:id", "id");
+                    o.ClaimActions.MapJsonKey("urn:botsin.space:username", "username");
+
+                    o.Events = new OAuthEvents {
+                        OnCreatingTicket = async context => {
+                            var request = new HttpRequestMessage(System.Net.Http.HttpMethod.Get, context.Options.UserInformationEndpoint);
+                            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+
+                            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                            var response = await context.Backchannel.SendAsync(request, context.HttpContext.RequestAborted);
+                            response.EnsureSuccessStatusCode();
+
+                            //context.HttpContext.Response.Cookies.Append("token", context.AccessToken);
+                            if (context.Options.SaveTokens) {
+                                context.Properties.StoreTokens(new[] {
+                                    new AuthenticationToken { Name = "access_token", Value = context.AccessToken }
+                                });
+                            }
+
+                            using var user = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+                            context.RunClaimActions(user.RootElement);
+                        },
+                        OnRemoteFailure = context => {
+                            context.HandleResponse();
+                            context.Response.Redirect("/Home/Error?message=" + context.Failure.Message);
+                            return Task.FromResult(0);
+                        }
+                    };
                 });
             services.AddSingleton<IDeviantArtAuth>(new DeviantArtAuth(
                 int.Parse(Configuration["Authentication:DeviantArt:ClientId"]),
