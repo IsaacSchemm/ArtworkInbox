f8c572b136bdb9e14eefe5b390b59473517909ff
Isaac Schemm <isaacschemm@gmail.com>
2020-01-31 16:55:01 +0000

    Add a stripped-down version of libertyernie's InkbunnyLib

---
+++ Inkbunny/Responses/InkbunnyLoginResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtworkInbox.Inkbunny.Responses {
+	public class InkbunnyLoginResponse : InkbunnyResponse {
+        public string sid;
+        public int user_id;
+        public string ratingsmask;
+	}
+}
