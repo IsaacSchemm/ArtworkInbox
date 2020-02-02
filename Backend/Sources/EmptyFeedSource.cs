f4b5763ba0e5e14e3ad381507469493b0278d99e
Isaac Schemm <isaacschemm@gmail.com>
2020-02-02 16:17:14 -0600

    Give EmptyFeedSource its own file

---
+++ Backend/Sources/EmptyFeedSource.cs
@@ -0,0 +1,22 @@
+﻿using ArtworkInbox.Backend.Types;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtworkInbox.Backend.Sources {
+    public class EmptyFeedSource : FeedSource {
+        public override Task<Author> GetAuthenticatedUserAsync() {
+            return Task.FromResult(new Author());
+        }
+
+        public override Task<FeedBatch> GetBatchAsync(string cursor) {
+            return Task.FromResult(new FeedBatch {
+                Cursor = null,
+                HasMore = false,
+                FeedItems = Enumerable.Empty<FeedItem>()
+            });
+        }
+
+        public override string GetNotificationsUrl() => "https://www.example.com";
+        public override string GetSubmitUrl() => "https://www.example.org";
+    }
+}
--- Backend/Sources/FeedSource.cs
+++ Backend/Sources/FeedSource.cs
@@ -47,25 +47,4 @@ namespace ArtworkInbox.Backend.Sources {
             };
         }
     }
-
-    public class EmptyFeedSource : FeedSource {
-        public override Task<Author> GetAuthenticatedUserAsync() {
-            return Task.FromResult(new Author {
-                Username = "Not logged in",
-                AvatarUrl = "https://upload.wikimedia.org/wikipedia/commons/c/ce/Transparent.gif",
-                ProfileUrl = "/Identity/Account/Manage/ExternalLogins"
-            });
-        }
-
-        public override Task<FeedBatch> GetBatchAsync(string cursor) {
-            return Task.FromResult(new FeedBatch {
-                Cursor = null,
-                HasMore = false,
-                FeedItems = Enumerable.Empty<FeedItem>()
-            });
-        }
-
-        public override string GetNotificationsUrl() => "https://www.example.com";
-        public override string GetSubmitUrl() => "https://www.example.org";
-    }
 }
