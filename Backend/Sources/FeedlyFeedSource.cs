f9f13b47f65d2aaf11b3746cb16c51d369ec287c
Isaac Schemm <isaacschemm@gmail.com>
2020-02-05 19:13:38 +0000

    Feedly support except oauth

---
+++ Backend/Sources/FeedlyFeedSource.cs
@@ -0,0 +1,82 @@
+﻿using ArtworkInbox.Backend.Types;
+using FeedlySharp;
+using FeedlySharp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ArtworkInbox.Backend.Sources {
+    public class FeedlyFeedSource : IFeedSource {
+        private readonly FeedlySharpHttpClient _client;
+
+        public FeedlyFeedSource(FeedlySharpHttpClient client) {
+            _client = client;
+        }
+
+        public async Task<Author> GetAuthenticatedUserAsync() {
+            var profile = await _client.GetProfile();
+            return new Author {
+                AvatarUrl = profile.Picture,
+                Username = profile.FullName
+            };
+        }
+
+        private static IEnumerable<FeedItem> Wrangle(IEnumerable<Entry> entries) {
+            foreach (var e in entries) {
+                if (e.Visual != null) {
+                    yield return new Artwork {
+                        Author = new Author {
+                            Username = e.Origin?.Title ?? e.Author
+                        },
+                        LinkUrl = e.Alternate
+                            .Where(x => x.Type == "text/html")
+                            .Select(x => x.Href)
+                            .DefaultIfEmpty(null)
+                            .First(),
+                        Thumbnails = new[] {
+                            new Types.Thumbnail {
+                                Url = e.Visual.Url,
+                                Width = e.Visual.Width,
+                                Height = e.Visual.Height
+                            }
+                        },
+                        Timestamp = e.Published,
+                        Title = e.Title
+                    };
+                } else {
+                    yield return new JournalEntry {
+                        Author = new Author {
+                            Username = e.Origin?.Title ?? e.Author
+                        },
+                        Html = e.Summary?.Content ?? "",
+                        LinkUrl = e.Alternate
+                            .Where(x => x.Type == "text/html")
+                            .Select(x => x.Href)
+                            .DefaultIfEmpty(null)
+                            .First(),
+                        Timestamp = e.Published
+                    };
+                }
+            }
+        }
+
+        public async Task<FeedBatch> GetBatchAsync(string cursor) {
+            var stream = await _client.GetStream(new StreamOptions {
+                StreamId = $"user/a9487394-85bc-47f0-b179-037d6af906aa/category/global.all",
+                Continuation = cursor,
+                UnreadOnly = true
+            });
+
+            return new FeedBatch {
+                Cursor = stream.Continuation,
+                HasMore = stream.Continuation != null,
+                FeedItems = Wrangle(stream.Items)
+            };
+        }
+
+        public string GetNotificationsUrl() => null;
+        public string GetSubmitUrl() => null;
+    }
+}
---
+++ Controllers/FeedlyController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ArtworkInbox.Backend;
+using ArtworkInbox.Backend.Sources;
+using ArtworkInbox.Data;
+using FeedlySharp;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace ArtworkInbox.Controllers {
+    public class FeedlyController : FeedController {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<HomeController> _logger;
+
+        public FeedlyController(UserManager<ApplicationUser> userManager, ApplicationDbContext context, ILogger<HomeController> logger) {
+            _userManager = userManager;
+            _context = context;
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            return RedirectToAction(nameof(Feed));
+        }
+
+        protected override Task<ApplicationUser> GetUserAsync() =>
+            _userManager.GetUserAsync(User);
+
+        protected override string GetSiteName() => "Feedly";
+
+        protected override async Task<IFeedSource> GetFeedSourceAsync() {
+            var client = new FeedlySharpHttpClient(new FeedlySharp.Models.FeedlyOptions {
+                UserID = "a9487394-85bc-47f0-b179-037d6af906aa",
+                AccessToken = "",
+                Domain = "https://cloud.feedly.com"
+            });
+            return new FeedlyFeedSource(client);
+        }
+
+        protected override async Task<DateTimeOffset> GetLastRead() {
+            return DateTimeOffset.MinValue;
+        }
+
+        protected override async Task SetLastRead(DateTimeOffset lastRead) {
+            throw new NotImplementedException();
+        }
+    }
+}
