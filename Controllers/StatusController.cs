471542bfc5a5cc981d004eb2a9386716c709753b
Isaac Schemm <isaacschemm@gmail.com>
2020-02-25 13:12:01 -0600

    Add better post UI

--- Controllers/StatusController.cs
+++ Controllers/StatusController.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using ArtworkInbox.Models;
-using Microsoft.AspNetCore.Authentication;
 using ArtworkInbox.Data;
 using Microsoft.AspNetCore.Identity;
 using DeviantArtFs;
 using Microsoft.EntityFrameworkCore;
 using ArtworkInbox.Backend.Sources;
+using ArtworkInbox.Models;
 
 namespace ArtworkInbox.Controllers {
     public class StatusController : Controller {
@@ -29,62 +27,66 @@ namespace ArtworkInbox.Controllers {
             _factory = factory;
         }
 
-        private async Task<IEnumerable<IPostDestination>> GetPostDestinationsAsync() {
-            var postDestinations = new List<IPostDestination>();
-
+        private async IAsyncEnumerable<IPostDestination> GetPostDestinationsAsync() {
             var userId = _userManager.GetUserId(User);
             var daTokens = await _context.UserDeviantArtTokens
                 .Where(t => t.UserId == userId)
                 .ToListAsync();
             foreach (var t in daTokens) {
                 var token = new DeviantArtTokenWrapper(_app, _context, t);
-                postDestinations.Add(new DeviantArtFeedSource(token));
+                yield return new DeviantArtFeedSource(token);
             }
 
             var tumblrTokens = await _context.UserTumblrTokens
                 .Where(t => t.UserId == userId)
                 .ToListAsync();
             foreach (var t in tumblrTokens) {
-                postDestinations.AddRange(await new TumblrFeedSource(_factory.Create(t)).GetPostDestinationsAsync());
+                foreach (var p in await new TumblrFeedSource(_factory.Create(t)).GetPostDestinationsAsync()) {
+                    yield return p;
+                }
             }
-
-            return postDestinations;
         }
 
+        private async IAsyncEnumerable<IPostDestination> GetSelectedPostDestinationsAsync(string[] profileUrls) {
+            await foreach (var p in GetPostDestinationsAsync())
+                if (profileUrls.Contains(await p.GetProfileUrlAsync()))
+                    yield return p;
+        }
+
+        [HttpGet]
         public async Task<IActionResult> Index() {
-            var postDestinations = await GetPostDestinationsAsync();
+            var postDestinations = new List<IPostDestination>();
+            await foreach (var p in GetPostDestinationsAsync())
+                postDestinations.Add(p);
 
             return View(postDestinations);
+        }
+
+        private async Task<StatusPostedModel> PostStatusAsync(IPostDestination p, string statusText) {
+            try {
+                Uri uri = await p.PostStatusAsync(statusText);
+                return new StatusPostedModel { Uris = new[] { uri } };
+            } catch (Exception ex) {
+                return new StatusPostedModel { Errors = new[] { $"Could not post to {await p.GetProfileUrlAsync()}: {ex.Message}" } };
+            }
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Post([FromForm]string[] profileUrls, [FromForm]string statusText) {
-            var postDestinations = await GetPostDestinationsAsync();
+            var postDestinations = new List<IPostDestination>();
+            await foreach (var p in GetSelectedPostDestinationsAsync(profileUrls))
+                postDestinations.Add(p);
 
-            var toUse = new List<IPostDestination>();
-            foreach (var p in postDestinations) {
-                try {
-                    if (profileUrls.Contains(await p.GetProfileUrlAsync())) {
-                        toUse.Add(p);
-                    }
-                } catch (Exception) { }
-            }
-
-            var uris = new List<Uri>();
-            var errors = new List<string>();
             if (!string.IsNullOrWhiteSpace(statusText)) {
-                foreach (var p in toUse) {
-                    try {
-                        Uri uri = await p.PostStatusAsync(statusText);
-                        uris.Add(uri);
-                    } catch (Exception ex) {
-                        errors.Add($"Could not post to {await p.GetProfileUrlAsync()}: {ex.Message}");
-                    }
-                }
+                var results = await Task.WhenAll(postDestinations.Select(x => PostStatusAsync(x, statusText)));
+                return View(new StatusPostedModel {
+                    Uris = results.SelectMany(x => x.Uris),
+                    Errors = results.SelectMany(x => x.Errors)
+                });
+            } else {
+                return RedirectToAction(nameof(Index));
             }
-
-            return Content(string.Join('\n', uris.Select(x => $"Posted to {x}").Concat(errors)));
         }
     }
 }
---
+++ Models/StatusPostedModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArtworkInbox.Models {
+    public class StatusPostedModel {
+        public IEnumerable<Uri> Uris { get; set; } = Enumerable.Empty<Uri>();
+        public IEnumerable<string> Errors { get; set; } = Enumerable.Empty<string>();
+    }
+}
