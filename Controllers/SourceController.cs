71e32ddae5c4447f48f59ff2b2475f546af84cbc
Isaac Schemm <isaacschemm@gmail.com>
2021-01-01 20:27:11 -0600

    Start working on new SourceController model

--- Controllers/SourceController.cs
+++ Controllers/SourceController.cs
@@ -1,102 +1,103 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using ArtworkInbox.Backend;
-//using ArtworkInbox.Backend.Types;
-//using ArtworkInbox.Data;
-//using ArtworkInbox.Models;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ArtworkInbox.Backend;
+using ArtworkInbox.Backend.Types;
+using ArtworkInbox.Data;
+using ArtworkInbox.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace ArtworkInbox.Controllers {
-//    [Authorize]
-//    public abstract class SourceController : Controller {
-//        protected abstract Task<ApplicationUser> GetUserAsync();
-//        protected abstract string SiteName { get; }
+namespace ArtworkInbox.Controllers {
+    [Authorize]
+    public abstract class SourceController : Controller {
+        protected abstract Task<ApplicationUser> GetUserAsync();
+        protected abstract string SiteName { get; }
 
-//        protected abstract Task<Author> GetAuthorAsync();
+        protected abstract Task<Author> GetAuthorAsync();
 
-//        protected abstract IAsyncEnumerable<FeedItem> GetFeedItemsAsync();
-//        protected abstract Task<DateTimeOffset> GetLastRead();
-//        protected abstract Task SetLastRead(DateTimeOffset lastRead);
+        protected abstract IAsyncEnumerable<FeedItem> GetFeedItemsAsync();
+        protected abstract Task<DateTimeOffset> GetLastRead();
+        protected abstract Task SetLastRead(DateTimeOffset lastRead);
 
-//        protected abstract string NotificationsUrl { get; }
-//        protected abstract Task<int?> GetNotificationsCountAsync();
+        protected abstract string NotificationsUrl { get; }
 
-//        protected abstract string SubmitUrl { get; }
+        protected abstract string SubmitUrl { get; }
 
-//        public async Task<IActionResult> Page(int offset = 0, int limit = 200, DateTimeOffset? latest = null) {
-//            try {
-//                var user = await GetUserAsync();
-//                var earliest = await GetLastRead();
+        protected virtual Task<string> GetNotificationsCountAsync() => Task.FromResult<string>(null);
 
-//                var source = GetFeedItemsAsync();
-//                if (user.HideReposts)
-//                    source = source.Where(x => x.RepostedFrom == null);
-//                if (user.HideMature)
-//                    source = source.Where(x => x.MatureContent == false);
-//                if (user.HideMatureThumbnails)
-//                    source = source.Select(i => {
-//                        if (i.MatureContent && i is Artwork a) {
-//                            return new Artwork {
-//                                Author = a.Author,
-//                                LinkUrl = a.LinkUrl,
-//                                MatureContent = a.MatureContent,
-//                                RepostedFrom = a.RepostedFrom,
-//                                Thumbnails = Enumerable.Empty<Thumbnail>(),
-//                                Timestamp = a.Timestamp,
-//                                Title = a.Title
-//                            };
-//                        } else {
-//                            return i;
-//                        }
-//                    });
+        public async Task<IActionResult> Page(int offset = 0, int limit = 200, DateTimeOffset? latest = null) {
+            try {
+                var user = await GetUserAsync();
+                var earliest = await GetLastRead();
 
-//                var feedItems = await source.Skip(offset).Take(limit).ToListAsync();
-//                bool hasMore = await source.Skip(offset + limit).AnyAsync();
+                var source = GetFeedItemsAsync();
+                if (user.HideReposts)
+                    source = source.Where(x => x.RepostedFrom == null);
+                if (user.HideMature)
+                    source = source.Where(x => x.MatureContent == false);
+                if (user.HideMatureThumbnails)
+                    source = source.Select(i => {
+                        if (i.MatureContent && i is Artwork a) {
+                            return new Artwork {
+                                Author = a.Author,
+                                LinkUrl = a.LinkUrl,
+                                MatureContent = a.MatureContent,
+                                RepostedFrom = a.RepostedFrom,
+                                Thumbnails = Enumerable.Empty<Thumbnail>(),
+                                Timestamp = a.Timestamp,
+                                Title = a.Title
+                            };
+                        } else {
+                            return i;
+                        }
+                    });
 
-//                return View(new SourceViewModel {
-//                    Latest = latest ?? feedItems.Select(x => x.Timestamp).DefaultIfEmpty(DateTimeOffset.MinValue).First(),
-//                    AuthenticatedUser = await GetAuthorAsync(),
-//                    FeedItems = feedItems,
-//                    NextOffset = offset + limit,
-//                    HasMore = hasMore,
-//                    NotificationsCount = await GetNotificationsCountAsync(),
-//                    NotificationsUrl = NotificationsUrl,
-//                    SubmitUrl = SubmitUrl
-//                });
-//            } catch (System.Text.Json.JsonException) {
-//                return JsonError();
-//            } catch (Newtonsoft.Json.JsonException) {
-//                return JsonError();
-//            } catch (FSharp.Json.JsonDeserializationError) {
-//                return JsonError();
-//            } catch (TooManyRequestsException) {
-//                return TooManyRequests();
-//            } catch (NoTokenException) {
-//                return NoToken();
-//            }
-//        }
+                var feedItems = await source.Skip(offset).Take(limit).ToListAsync();
+                bool hasMore = await source.Skip(offset + limit).AnyAsync();
 
-//        public IActionResult JsonError() {
-//            ViewBag.SiteName = SiteName;
-//            return View("JsonError");
-//        }
+                return View(new SourceViewModel {
+                    Latest = latest ?? feedItems.Select(x => x.Timestamp).DefaultIfEmpty(DateTimeOffset.MinValue).First(),
+                    AuthenticatedUser = await GetAuthorAsync(),
+                    FeedItems = feedItems,
+                    NextOffset = offset + limit,
+                    HasMore = hasMore,
+                    NotificationsCount = await GetNotificationsCountAsync(),
+                    NotificationsUrl = NotificationsUrl,
+                    SubmitUrl = SubmitUrl
+                });
+            } catch (System.Text.Json.JsonException) {
+                return JsonError();
+            } catch (Newtonsoft.Json.JsonException) {
+                return JsonError();
+            } catch (FSharp.Json.JsonDeserializationError) {
+                return JsonError();
+            } catch (TooManyRequestsException) {
+                return TooManyRequests();
+            } catch (NoTokenException) {
+                return NoToken();
+            }
+        }
 
-//        public IActionResult TooManyRequests() {
-//            ViewBag.SiteName = SiteName;
-//            return View("TooManyRequests");
-//        }
+        public IActionResult JsonError() {
+            ViewBag.SiteName = SiteName;
+            return View("JsonError");
+        }
 
-//        public IActionResult NoToken() {
-//            ViewBag.SiteName = SiteName;
-//            return View("NoToken");
-//        }
+        public IActionResult TooManyRequests() {
+            ViewBag.SiteName = SiteName;
+            return View("TooManyRequests");
+        }
 
-//        public async Task<IActionResult> MarkAsRead(DateTimeOffset latest) {
-//            await SetLastRead(latest);
-//            return RedirectToAction(nameof(Page));
-//        }
-//    }
-//}
+        public IActionResult NoToken() {
+            ViewBag.SiteName = SiteName;
+            return View("NoToken");
+        }
+
+        public async Task<IActionResult> MarkAsRead(DateTimeOffset latest) {
+            await SetLastRead(latest);
+            return RedirectToAction(nameof(Page));
+        }
+    }
+}
