7718c4c538da7b018ac009a0f19f03f072f53ab2
Isaac Schemm <isaacschemm@gmail.com>
2022-12-26 17:45:55 -0600

    Remove database start/stop

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,44 +1,12 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ArtworkInbox.Models;
-using System.Threading.Tasks;
-using System.Net.Http;
-using System;
-using Microsoft.FSharp.Collections;
-using System.Net.Http.Json;
-using System.Linq;
 using ArtworkInbox.Data;
 
 namespace ArtworkInbox.Controllers {
     public class HomeController : Controller {
-        private readonly ApplicationDbContext _context;
-        private readonly HttpClient _httpClient;
-
-        public HomeController(ApplicationDbContext context, IHttpClientFactory httpClientFactory) {
-            _context = context;
-            _httpClient = httpClientFactory.CreateClient();
-            _httpClient.BaseAddress = new Uri("https://artworkinbox-db-management.azurewebsites.net");
-        }
-
-        public async Task<IActionResult> Index() {
-            //await _context.Database.EnsureCreatedAsync();
-            try {
-                var powerStates = await _httpClient.GetFromJsonAsync<FSharpList<string>>("/api/power-states");
-                ViewBag.DatabaseServerStatus = $"{powerStates}";
-                ViewBag.CanStartDatabaseServer = !powerStates.Contains("PowerState/running");
-            } catch (Exception) {
-                ViewBag.DatabaseServerStatus = "unknown";
-                ViewBag.CanStartDatabaseServer = false;
-            }
+        public IActionResult Index() {
             return View();
-        }
-
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> StartDatabase() {
-            var response = await _httpClient.PostAsync("/api/start", null);
-            if (!response.IsSuccessStatusCode)
-                return StatusCode(502);
-            return RedirectToAction(nameof(Index));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
