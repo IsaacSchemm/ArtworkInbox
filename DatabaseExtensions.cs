f755535cd8b32adde8c827a4641ea52a541323bc
Isaac Schemm <isaacschemm@gmail.com>
2022-12-26 18:57:44 -0600

    Workaround for Cosmos DB backend

---
+++ DatabaseExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtworkInbox {
+    public static class DatabaseExtensions {
+        public static async Task<bool> ListAnyAsync<T>(this IQueryable<T> q) {
+            var list = await q.Take(1).ToListAsync();
+            return list.Any();
+        }
+    }
+}
