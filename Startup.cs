d97fb21fdba846c9bb4aff2a37da797fb6c83efa
Isaac Schemm <isaacschemm@gmail.com>
2024-06-13 22:10:08 -0500

    Remove packages

--- Startup.cs
+++ Startup.cs
@@ -13,7 +13,7 @@ namespace ArtworkInbox {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
